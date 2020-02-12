cd486f0e94570dc82f18f8d2ec6308955597dfe3
wlukla <vladislav.danilochkin@gmail.com>
2020-02-12 17:02:18 +0200

    feat: add context for swapiService

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,38 +3,32 @@ import React from 'react';
 import Header from '../header';
 // import RandomPlanet from '../random-planet';
 // import PeoplePage from '../people-page';
-import ErrorIndicator from '../error-indicator';
+import ErrorBoundary from '../error-boundary';
 import SwapiService from '../../services/swapi-service';
 import { PersonList, StarshipList, PersonDetails } from '../sw-components';
+
+import { SwapiServiceProvider } from '../swapi-service-context';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.swapiService = new SwapiService();
-    this.state = {
-      hasError: false,
-    };
-  }
-
-  componentDidCatch() {
-    this.setState({ hasError: true });
+    this.state = {};
   }
 
   render() {
-    const { hasError } = this.state;
-
-    if (hasError) {
-      return <ErrorIndicator />;
-    }
-
     return (
-      <div className="container">
-        <Header />
+      <ErrorBoundary>
+        <SwapiServiceProvider value={this.swapiService}>
+          <div className="container">
+            <Header />
+            <PersonDetails itemId={4} />
+            <PersonList />
 
-        <PersonList />
-
-        <StarshipList />
-      </div>
+            <StarshipList />
+          </div>
+        </SwapiServiceProvider>
+      </ErrorBoundary>
     );
   }
 }
--- src/components/sw-components/details.jsx
+++ src/components/sw-components/details.jsx
@@ -1,55 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ItemDetails from '../item-details';
-import SwapiService from '../../services/swapi-service';
 import Record from '../record';
-
-const swapi = new SwapiService();
-
-const {
-  getPerson,
-  getPlanet,
-  getStarship,
-  getPersonImage,
-  getPlanetImage,
-  getStarshipImage,
-} = swapi;
+import { SwapiServiceConsumer } from '../swapi-service-context';
 
 const PersonDetails = ({ itemId }) => (
-  <ItemDetails
-    itemId={itemId}
-    getData={getPerson}
-    getImageURL={getPersonImage}
-  >
-    <Record label="Gender" field="gender" />
-    <Record label="Birth year" field="birthYear" />
-    <Record label="Eye color" field="eyeColor" />
-  </ItemDetails>
+  <SwapiServiceConsumer>
+    {
+      ({ getPerson, getPersonImage }) => (
+        <ItemDetails
+          itemId={itemId}
+          getData={getPerson}
+          getImageURL={getPersonImage}
+        >
+          <Record label="Gender" field="gender" />
+          <Record label="Birth year" field="birthYear" />
+          <Record label="Eye color" field="eyeColor" />
+        </ItemDetails>
+      )
+    }
+  </SwapiServiceConsumer>
 );
 
 const PlanetDetails = ({ itemId }) => (
-  <ItemDetails
-    itemId={itemId}
-    getData={getPlanet}
-    getImageURL={getPlanetImage}
-  >
-    <Record label="Population" field="population" />
-    <Record label="Rotation period" field="rotationPeriod" />
-    <Record label="Diameter" field="diameter" />
-  </ItemDetails>
+  <SwapiServiceConsumer>
+    {
+      ({ getPlanet, getPlanetImage }) => (
+        <ItemDetails
+          itemId={itemId}
+          getData={getPlanet}
+          getImageURL={getPlanetImage}
+        >
+          <Record label="Population" field="population" />
+          <Record label="Rotation period" field="rotationPeriod" />
+          <Record label="Diameter" field="diameter" />
+        </ItemDetails>
+      )
+    }
+  </SwapiServiceConsumer>
+
 );
 
 const StarshipDetails = ({ itemId }) => (
-  <ItemDetails
-    itemId={itemId}
-    getData={getStarship}
-    getImageURL={getStarshipImage}
-  >
-    <Record label="Manufacturer" field="manufacturer" />
-    <Record label="Cost" field="constInCredits" />
-    <Record label="Length" field="length" />
-    <Record label="Cargo capacity" field="cargoCapacity" />
-  </ItemDetails>
+  <SwapiServiceConsumer>
+    {
+      ({ getStarship, getStarshipImage }) => (
+        <ItemDetails
+          itemId={itemId}
+          getData={getStarship}
+          getImageURL={getStarshipImage}
+        >
+          <Record label="Manufacturer" field="manufacturer" />
+          <Record label="Cost" field="constInCredits" />
+          <Record label="Length" field="length" />
+          <Record label="Cargo capacity" field="cargoCapacity" />
+        </ItemDetails>
+      )
+    }
+  </SwapiServiceConsumer>
 );
 
 const propTypes = {
