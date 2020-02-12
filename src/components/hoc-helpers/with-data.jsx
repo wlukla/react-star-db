25517bb4a0bb75d09e04c4e66417559cdc716e52
wlukla <vladislav.danilochkin@gmail.com>
2020-02-12 22:39:08 +0200

    feat: add ErrorIndicator to withData hoc

--- src/components/hoc-helpers/with-data.jsx
+++ src/components/hoc-helpers/with-data.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Loader from '../loader/loader';
+import ErrorIndicator from '../error-indicator';
 
 const withData = (View) => {
   class WithData extends React.Component {
@@ -10,20 +11,44 @@ const withData = (View) => {
 
       this.state = {
         data: null,
+        loading: true,
+        error: false,
       };
     }
 
     componentDidMount() {
+      this.update();
+    }
+
+    update() {
       const { getData } = this.props;
+
+      this.setState({
+        loading: true,
+        error: false,
+      });
       getData()
-        .then((data) => this.setState({ data }));
+        .then((data) => this.setState({
+          data,
+          loading: false,
+        }))
+        .catch(() => {
+          this.setState({
+            loading: false,
+            error: true,
+          });
+        });
     }
 
     render() {
-      const { data } = this.state;
+      const { data, loading, error } = this.state;
 
-      if (!data) {
+      if (loading) {
         return <Loader />;
+      }
+
+      if (error) {
+        return <ErrorIndicator />;
       }
 
       return <View {...this.props} data={data} />;
